979af432222f1d2db35a1fc9cf83457668863d7b
Mustafa kart <mustafakart000@gmail.com>
2022-10-16 22:55:11 +0300

    new 03

---
+++ src/main/java/arraylist/arraylistday16/Day17ArrayList/ArrayList03.java
@@ -0,0 +1,7 @@
+package arraylist.arraylistday16.Day17ArrayList;
+
+public class ArrayList03 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/arraylist/arraylistday16/Day17ArrayList/ArrayLİst01.java
+++ src/main/java/arraylist/arraylistday16/Day17ArrayList/ArrayLİst01.java
@@ -2,6 +2,7 @@ package arraylist.arraylistday16.Day17ArrayList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ArrayLİst01 {
     public static void main(String[] args) {
@@ -73,9 +74,9 @@ public class ArrayLİst01 {
         // Note: iki arrayList'in eşit olması için, elemanlar eşit olmalı ve aynı elemanlar aynı index de olmalıdır
         List<Character> m = new ArrayList<>();
         m.add('x');
-        m.add('y');
         m.add('z');
-        m.add('t');
+        m.add('y');
+
 
         List<Character> n = new ArrayList<>();
         n.add('x');
@@ -98,9 +99,89 @@ public class ArrayLİst01 {
 
                 }if (counter == 0) {
             System.out.println("listler eşittir");
-
                 }
 
+
+            //2.yol:
+        boolean esitmi = m.equals(n);
+        if(esitmi){
+            System.out.println("Listler eşittir");
+        }
+        else {
+            System.out.println("Listler eşit değildir");
+        }
+        //Example 1: Verilen bir listteki elemanlari tekrarsiz olarak yaziniz
+//           [2, 3, 2, 2, 5] ==> [2, 3, 5]
+
+
+        Scanner scan= new Scanner(System.in);
+
+        List<Integer> sayilar = new ArrayList<>();
+        sayilar.add(2);
+        sayilar.add(3);
+        sayilar.add(2);
+        sayilar.add(2);
+        sayilar.add(5);
+        List<Integer> newSayilar=new ArrayList<>();
+        newSayilar.add(2);
+        newSayilar.add(3);
+        newSayilar.add(7);
+        newSayilar.add(4);
+        newSayilar.add(5);
+        int c,b;
+        for (int i=0; i< a.size(); i++) {
+            c=sayilar.lastIndexOf(sayilar.size());
+            b=sayilar.indexOf(i);
+            if (c==b){
+                newSayilar.add(c);
+            }
+        }System.out.println(newSayilar);
+
+
+
+
+        //Example 1: Verilen bir listteki elemanlari tekrarsiz olarak yaziniz
+        //           [2, 3, 2, 2, 5] ==> [2, 3, 5]
+        List<Integer> a1 = new ArrayList<>();
+        a1.add(2);
+        a1.add(3);
+        a1.add(2);
+        a1.add(2);
+        a1.add(5);
+
+        List<Integer> yeni = new ArrayList<>();
+        // [2, 3, 2, 2, 5]
+        for(Integer w : a1){
+            if(!yeni.contains(w)){
+                yeni.add(w);
+            }
+        }
+        System.out.println(yeni);// [2, 3, 5]
+
+        //Example 2: Kullanicidan bir harf aliniz, harf sizdeki List'in icinde varsa o harfi "Bulundu" ya ceviriniz yoksa o harfi List'e ekleyiniz
+        //           [A, K, R, S]  ==> R  ==> [A, K, Bulundu, S]
+        //           [A, K, R, S]  ==> X  ==> [A, K, R, S, X]
+        Scanner input = new Scanner(System.in);
+
+        String harf = "";
+
+        List<String> c1 = new ArrayList<>();
+        c1.add("A");
+        c1.add("K");
+        c1.add("R");
+        c1.add("S");
+
+        do {
+            System.out.println("Lutfen bir harf giriniz");
+            harf = input.next().substring(0, 1);
+
+            if (c1.contains(harf)) {
+                c1.set(c1.indexOf(harf), "Bulundu");
+            } else if(!harf.equals("Q")){
+                c1.add(harf);
+            }
+            System.out.println(c1);
+        }while(!harf.equals("Q"));
 
 
 
