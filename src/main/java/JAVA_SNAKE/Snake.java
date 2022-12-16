1abb2fe3c341897f8ecf545b466a1d2bb67f236e
Mustafa kart <mustafakart000@gmail.com>
2022-12-17 00:00:01 +0300

    Advanced Java Day01

---
+++ src/main/java/JAVA_SNAKE/Snake.java
@@ -0,0 +1,25 @@
+package JAVA_SNAKE;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Snake extends JFrame {
+    public Snake(){
+        initUI();
+    }
+    private void initUI(){
+        add(new Board());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            JFrame ex= new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
