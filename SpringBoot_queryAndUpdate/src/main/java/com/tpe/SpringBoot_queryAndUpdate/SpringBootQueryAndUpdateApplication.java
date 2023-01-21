917d993c70f3252ba6182e17715afc0c90e2c56b
Mustafa kart <mustafakart000@gmail.com>
2023-01-21 12:15:34 +0300

    springBootRepoQuery

---
+++ SpringBoot_queryAndUpdate/src/main/java/com/tpe/SpringBoot_queryAndUpdate/SpringBootQueryAndUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.tpe.SpringBoot_queryAndUpdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQueryAndUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQueryAndUpdateApplication.class, args);
+	}
+
+}
--- springbootintro/src/main/java/com/tpe/deneme.java
+++
@@ -1,7 +0,0 @@
-package com.tpe;
-
-public class deneme {
-    public static void main(String[] args) {
-        System.out.println("deneme");
-    }
-}
