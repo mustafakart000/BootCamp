ac85639a2a72a69e5409446e61b5a12b256c64da
Mustafa kart <mustafakart000@gmail.com>
2023-09-23 19:40:17 +0300

    springboot-ecommerce-step-by-step->Qualifier Primary ConditionOn

--- learn-maven-spring-framework/src/main/java/howToDoBeanConfiguration/ConfigurationBeanByMainMethod.java
+++ learn-maven-spring-framework/src/main/java/howToDoBeanConfiguration/ConfigurationBeanByMainMethod.java
@@ -1,4 +1,11 @@
 package howToDoBeanConfiguration;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConfigurationBeanByMainMethod {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationBeanByMainMethod.class,args);
+    }
 }
