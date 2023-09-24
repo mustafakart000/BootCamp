b345994a4c91260d91a42deb8c863194efa5a63c
Mustafa kart <mustafakart000@gmail.com>
2023-09-24 23:29:41 +0300

    version1.1

--- learn-maven-spring-framework/src/main/java/howToDoBeanConfiguration/configuration/AppConfugiration.java
+++ learn-maven-spring-framework/src/main/java/howToDoBeanConfiguration/configuration/AppConfugiration.java
@@ -5,11 +5,15 @@ import howToDoBeanConfiguration.PlaneVehicleService;
 import howToDoBeanConfiguration.VehicleService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
 public class AppConfugiration {
 
     @Bean
+    @Primary
     public VehicleService carVehicleServiceConfiguration(){
         return new CarVehicleService();
     }
